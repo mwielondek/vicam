71ced40fa9ac4738a40868ef342faefa0d42d70d
Fredrik Sommar <fred.sommar+github@gmail.com>
2014-04-02 14:52:27 +0200

    Added interface bodies for DAOs

--- camera/src/main/java/com/dreamteam/vicam/model/database/CameraDAO.java
+++ camera/src/main/java/com/dreamteam/vicam/model/database/CameraDAO.java
@@ -1,8 +1,16 @@
 package com.dreamteam.vicam.model.database;
 
+import com.dreamteam.vicam.model.pojo.Camera;
+
+import java.util.List;
+
 /**
  * Created by fsommar on 2014-04-01.
  */
 public interface CameraDAO {
-
+  public int insertCamera(Camera camera);
+  public Camera findCamera(int id);
+  public boolean updateCamera(Camera camera);
+  public boolean deleteCamera(int id);
+  public List<Camera> getCameras();
 }
--- camera/src/main/java/com/dreamteam/vicam/model/database/PresetDAO.java
+++ camera/src/main/java/com/dreamteam/vicam/model/database/PresetDAO.java
@@ -1,8 +1,16 @@
 package com.dreamteam.vicam.model.database;
 
+import com.dreamteam.vicam.model.pojo.Preset;
+
+import java.util.List;
+
 /**
  * Created by fsommar on 2014-04-01.
  */
 public interface PresetDAO {
-
+  public int insertPreset(Preset preset);
+  public Preset findPreset(int id);
+  public boolean updatePreset(Preset preset);
+  public boolean deletePreset(int id);
+  public List<Preset> getPresets();
 }
