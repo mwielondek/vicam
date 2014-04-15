ba205c5102522491c14093d689a151d7afba488c
Fredrik Sommar <fred.sommar+github@gmail.com>
2014-04-15 23:29:25 +0200

    Fixed formatting in MainActivity.

--- camera/src/main/java/com/dreamteam/vicam/view/MainActivity.java
+++ camera/src/main/java/com/dreamteam/vicam/view/MainActivity.java
@@ -17,126 +17,122 @@ import com.dreamteam.camera.R;
 
 public class MainActivity extends Activity {
 
-        private String[] mPlanetTitles;
-        private DrawerLayout mDrawerLayout;
-        private ListView mDrawerList;
-        private CharSequence mTitle;
-        private ActionBarDrawerToggle mDrawerToggle;
+  private String[] mPlanetTitles;
+  private DrawerLayout mDrawerLayout;
+  private ListView mDrawerList;
+  private CharSequence mTitle;
+  private ActionBarDrawerToggle mDrawerToggle;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-            mTitle = "Change presets";
+    mTitle = "Change presets";
 
-            mPlanetTitles = new String[]{"Preset 1", "Preset 2", "Preset 3"};
-            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-            mDrawerList = (ListView) findViewById(R.id.left_drawer);
+    mPlanetTitles = new String[]{"Preset 1", "Preset 2", "Preset 3"};
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-// Set the adapter for the list view
-            mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                    R.layout.drawer_list_item, mPlanetTitles));
-// Set the list's click listener
-            mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    // Set the adapter for the list view
+    mDrawerList.setAdapter(
+        new ArrayAdapter<String>(this, R.layout.drawer_list_item, mPlanetTitles));
+    // Set the list's click listener
+    mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-            mDrawerToggle = new ActionBarDrawerToggle(
-                    this, /* host Activity */
-                    mDrawerLayout, /* DrawerLayout object */
-                    R.drawable.ic_drawer, /* nav drawer icon to replace 'Up' caret */
-                    R.string.drawer_open, /* "open drawer" description */
-                    R.string.drawer_close /* "close drawer" description */
-            ) {
+    mDrawerToggle = new ActionBarDrawerToggle(
+        this, /* host Activity */
+        mDrawerLayout, /* DrawerLayout object */
+        R.drawable.ic_drawer, /* nav drawer icon to replace 'Up' caret */
+        R.string.drawer_open, /* "open drawer" description */
+        R.string.drawer_close /* "close drawer" description */
+    ) {
+      /**
+       * Called when a drawer has settled in a completely closed state.
+       */
+      @Override
+      public void onDrawerClosed(View view) {
+        getActionBar().setTitle(mTitle);
+      }
 
+      /**
+       * Called when a drawer has settled in a completely open state.
+       */
+      @Override
+      public void onDrawerOpened(View drawerView) {
+        getActionBar().setTitle(mTitle);
+      }
+    };
 
+    // Set the drawer toggle as the DrawerListener
+    mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-                /** Called when a drawer has settled in a completely closed state. */
-                public void onDrawerClosed(View view) {
-                    getActionBar().setTitle(mTitle);
-                }
+    getActionBar().setDisplayHomeAsUpEnabled(true);
+    getActionBar().setHomeButtonEnabled(true);
+  }
 
-                /** Called when a drawer has settled in a completely open state. */
-                public void onDrawerOpened(View drawerView) {
-                    getActionBar().setTitle(mTitle);
-                }
-            };
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    //getActionBar().setDisplayShowHomeEnabled(false);
 
-// Set the drawer toggle as the DrawerListener
-            mDrawerLayout.setDrawerListener(mDrawerToggle);
+    // Removes the icon
+    getActionBar().setIcon(android.R.color.transparent);
 
-            getActionBar().setDisplayHomeAsUpEnabled(true);
-            getActionBar().setHomeButtonEnabled(true);
+    // getActionBar().setDisplayShowTitleEnabled(false);
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
 
+  @Override
+  protected void onPostCreate(Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+    // Sync the toggle state after onRestoreInstanceState has occurred.
+    mDrawerToggle.syncState();
+  }
 
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    mDrawerToggle.onConfigurationChanged(newConfig);
+  }
 
-        }
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Pass the event to ActionBarDrawerToggle, if it returns
+    // true, then it has handled the app icon touch event
+    if (mDrawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    // Handle your other action bar items...
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-// Inflate the menu; this adds items to the action bar if it is present.
-            //getActionBar().setDisplayShowHomeEnabled(false);
+    return super.onOptionsItemSelected(item);
+  }
 
-            // Removes the icon
-            getActionBar().setIcon(android.R.color.transparent);
-
-
-           // getActionBar().setDisplayShowTitleEnabled(false);
-            getMenuInflater().inflate(R.menu.main, menu);
-            return true;
-        }
-
-        @Override
-        protected void onPostCreate(Bundle savedInstanceState) {
-            super.onPostCreate(savedInstanceState);
-// Sync the toggle state after onRestoreInstanceState has occurred.
-            mDrawerToggle.syncState();
-        }
-
-        @Override
-        public void onConfigurationChanged(Configuration newConfig) {
-            super.onConfigurationChanged(newConfig);
-            mDrawerToggle.onConfigurationChanged(newConfig);
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-// Pass the event to ActionBarDrawerToggle, if it returns
-// true, then it has handled the app icon touch event
-            if (mDrawerToggle.onOptionsItemSelected(item)) {
-                return true;
-            }
-// Handle your other action bar items...
-
-            return super.onOptionsItemSelected(item);
-        }
-
-        /**
-         * Swaps fragments in the main ontent view
-         */
-        private void selectItem(int position) {
-           Toast.makeText(this, R.string.app_name, Toast.LENGTH_SHORT).show();
-
+  /**
+   * Swaps fragments in the main content view
+   */
+  private void selectItem(int position) {
+    Toast.makeText(this, R.string.app_name, Toast.LENGTH_SHORT).show();
 
 // Highlight the selected item, update the title, and close the drawer
-            mDrawerList.setItemChecked(position, true);
-            setTitle(mPlanetTitles[position]);
-            mDrawerLayout.closeDrawer(mDrawerList);
-        }
+    mDrawerList.setItemChecked(position, true);
+    setTitle(mPlanetTitles[position]);
+    mDrawerLayout.closeDrawer(mDrawerList);
+  }
 
-        @Override
-        public void setTitle(CharSequence title) {
-            mTitle = title;
-            getActionBar().setTitle(mTitle);
+  @Override
+  public void setTitle(CharSequence title) {
+    mTitle = title;
+    getActionBar().setTitle(mTitle);
+  }
 
+  private class DrawerItemClickListener implements ListView.OnItemClickListener {
 
-
-        }
-
-        private class DrawerItemClickListener implements ListView.OnItemClickListener {
-            @Override
-            public void onItemClick(AdapterView parent, View view, int position, long id) {
-                selectItem(position);
-            }
-        }
-
+    @Override
+    public void onItemClick(AdapterView parent, View view, int position, long id) {
+      selectItem(position);
     }
+  }
+
+}
