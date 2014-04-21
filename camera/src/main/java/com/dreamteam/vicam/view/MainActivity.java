f2937cac423f0697ee92b91eaa3c64b3418dc112
Fredrik Sommar <fred.sommar+github@gmail.com>
2014-04-21 22:53:46 +0200

    Major clean up. Removed lots of dead code and extracted listeners to their own private classes.
    ButterKnife is used to inject the views in order to declutter onCreate.

--- camera/src/main/java/com/dreamteam/vicam/view/MainActivity.java
+++ camera/src/main/java/com/dreamteam/vicam/view/MainActivity.java
@@ -1,6 +1,5 @@
 package com.dreamteam.vicam.view;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,26 +14,39 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.SeekBar;
 
 import com.dreamteam.camera.R;
 import com.dreamteam.vicam.model.pojo.Camera;
+import com.dreamteam.vicam.model.pojo.Preset;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
-public class MainActivity extends Activity implements ActionBar.OnNavigationListener {
+public class MainActivity extends Activity {
 
-  private String[] mPlanetTitles;
-  private DrawerLayout mDrawerLayout;
-  private ListView mDrawerList;
   private CharSequence mTitle;
-  private ActionBarDrawerToggle mDrawerToggle;
-  private MenuItem mSpinnerItem;
-  private ArrayAdapter changeCameraAdapter;
+  private Preset[] mPresets;
 
+  private ActionBarDrawerToggle mDrawerToggle;
+  private ArrayAdapter<Camera> mCameraAdapter;
+  private ArrayAdapter<Preset> mPresetAdapter;
+
+  @InjectView(R.id.drawer_layout)
+  DrawerLayout mDrawerLayout;
+  @InjectView(R.id.navigation_drawer)
+  ListView mDrawerList;
+  @InjectView(R.id.focus_seekbar)
+  SeekBar mFocusSeekBar;
+  @InjectView(R.id.zoom_seekbar)
+  SeekBar mZoomSeekBar;
+  @InjectView(R.id.focus_value)
+  TextView mFocusValue;
+  @InjectView(R.id.zoom_value)
+  TextView mZoomValue;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -48,49 +60,16 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
 
     mTitle = getString(R.string.app_name);
 
-    mPlanetTitles = new String[]{"Preset 1", "Preset 2", "Preset 3"};
+    // Temporary dummy data
+    mPresets = new Preset[]{
+        new Preset("Preset 1", null), new Preset("Preset 2", null),
+        new Preset("Preset 3", null)};
 
-    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-    mDrawerList = (ListView) findViewById(R.id.navigation_drawer);
-
-    SeekBar mSeekBarFocus = (SeekBar)findViewById(R.id.focus_seekbar);
-    SeekBar mSeekBarZoom = (SeekBar)findViewById(R.id.zoom_seekbar);
-    final TextView seekBarFocusValue = (TextView)findViewById(R.id.focus_value);
-    final TextView seekBarZoomValue = (TextView)findViewById(R.id.zoom_value);
-
-
-
-    // Set the changeCameraAdapter for the list view
-    mDrawerList.setAdapter(
-        new ArrayAdapter<String>(this, R.layout.drawer_list_item, mPlanetTitles));
-    // Set the list's click listener
+    mPresetAdapter = new ArrayAdapter<Preset>(this, R.layout.drawer_list_item, mPresets);
+    mDrawerList.setAdapter(mPresetAdapter);
     mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-    mDrawerToggle = new ActionBarDrawerToggle(
-        this, /* host Activity */
-        mDrawerLayout, /* DrawerLayout object */
-        R.drawable.ic_drawer, /* nav drawer icon to replace 'Up' caret */
-        R.string.drawer_open, /* "open drawer" description */
-        R.string.drawer_close /* "close drawer" description */
-    ) {
-      /**
-       * Called when a drawer has settled in a completely closed state.
-       */
-      @Override
-      public void onDrawerClosed(View view) {
-        getActionBar().setTitle(getString(R.string.app_name));
-      }
-
-      /**
-       * Called when a drawer has settled in a completely open state.
-       */
-      @Override
-      public void onDrawerOpened(View drawerView) {
-        getActionBar().setTitle(getString(R.string.change_preset));
-      }
-    };
-
-    // Set the drawer toggle as the DrawerListener
+    mDrawerToggle = new DrawerToggle(this, mDrawerLayout);
     mDrawerLayout.setDrawerListener(mDrawerToggle);
 
     getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -99,80 +78,37 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
     // Hide the action bar title
     getActionBar().setDisplayShowTitleEnabled(true);
 
-    changeCameraAdapter =
-        new ArrayAdapter<Camera>(this, R.layout.change_camera_spinner);
-    changeCameraAdapter.add(new Camera("127.0.0.1", "Test :3", null));
-    changeCameraAdapter.add(new Camera("localhost", "Test2 >:3", null));
+    mCameraAdapter = new ArrayAdapter<Camera>(this, R.layout.change_camera_spinner);
+    mCameraAdapter.add(new Camera("127.0.0.1", "Test :3", null));
+    mCameraAdapter.add(new Camera("localhost", "Test2 >:3", null));
 
-    mSeekBarFocus.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-      @Override
-      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-        seekBarFocusValue.setText(String.valueOf(progress));
-
-      }
-
-      @Override
-      public void onStartTrackingTouch(SeekBar seekBar) {
-
-      }
-
-      @Override
-      public void onStopTrackingTouch(SeekBar seekBar) {
-
-      }
-    });
-
-    mSeekBarZoom.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-      @Override
-      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-        seekBarZoomValue.setText(String.valueOf(progress));
-
-      }
-
-      @Override
-      public void onStartTrackingTouch(SeekBar seekBar) {
-
-      }
-
-      @Override
-      public void onStopTrackingTouch(SeekBar seekBar) {
-
-      }
-    });
+    mFocusSeekBar.setOnSeekBarChangeListener(new SeekBarChangeListener(mFocusValue));
+    mZoomSeekBar.setOnSeekBarChangeListener(new SeekBarChangeListener(mZoomValue));
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-// Inflate the menu; this adds items to the action bar if it is present.
-
+    // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
 
-    mSpinnerItem = menu.findItem(R.id.action_change_camera);
-    View spinnerView = mSpinnerItem.getActionView();
+    MenuItem cameraSpinner = menu.findItem(R.id.action_change_camera);
+    View spinnerView = cameraSpinner.getActionView();
     if (spinnerView instanceof Spinner) {
       final Spinner spinner = (Spinner) spinnerView;
-      spinner.setAdapter(changeCameraAdapter);
+      spinner.setAdapter(mCameraAdapter);
 
       spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
         @Override
         public void onItemSelected(AdapterView<?> arg0, View arg1,
                                    int arg2, long arg3) {
-
         }
 
         @Override
         public void onNothingSelected(AdapterView<?> arg0) {
-
-
         }
       });
 
     }
-    // getActionBar().setDisplayShowTitleEnabled(false);
-
     return true;
   }
 
@@ -206,22 +142,22 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
     }
   }
 
+  @Override
+  public void setTitle(CharSequence title) {
+    mTitle = title;
+    getActionBar().setTitle(mTitle);
+  }
+
   /**
-   * Swaps fragments in the main ontent view
+   * Swaps fragments in the main content view
    */
   private void selectItem(int position) {
     Toast.makeText(this, R.string.app_name, Toast.LENGTH_SHORT).show();
 
     // Highlight the selected item, update the title, and close the drawer
     mDrawerList.setItemChecked(position, true);
-    setTitle(mPlanetTitles[position]);
+    setTitle(mPresets[position].getName());
     mDrawerLayout.closeDrawer(mDrawerList);
-  }
-
-  @Override
-  public void setTitle(CharSequence title) {
-    mTitle = title;
-    getActionBar().setTitle(mTitle);
   }
 
   private class DrawerItemClickListener implements ListView.OnItemClickListener {
@@ -232,17 +168,52 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
     }
   }
 
+  private class DrawerToggle extends ActionBarDrawerToggle {
 
+    private DrawerToggle(Activity activity, DrawerLayout drawerLayout) {
+      super(activity, drawerLayout, R.drawable.ic_drawer, R.string.drawer_open,
+            R.string.drawer_close);
+    }
 
+    /**
+     * Called when a drawer has settled in a completely closed state.
+     */
+    @Override
+    public void onDrawerClosed(View view) {
+      getActionBar().setTitle(getString(R.string.app_name));
+    }
 
+    /**
+     * Called when a drawer has settled in a completely open state.
+     */
+    @Override
+    public void onDrawerOpened(View view) {
+      getActionBar().setTitle(getString(R.string.change_preset));
+    }
+  }
 
-  /**
-   * Actionbar navigation item select listener
-   */
-  @Override
-  public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-    // Action to be taken after selecting a spinner item
-    return false;
+  private class SeekBarChangeListener implements SeekBar.OnSeekBarChangeListener {
+
+    private final TextView mTextValue;
+
+    private SeekBarChangeListener(TextView mTextValue) {
+      this.mTextValue = mTextValue;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+      mTextValue.setText(Integer.toString(progress));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
   }
 
 }
