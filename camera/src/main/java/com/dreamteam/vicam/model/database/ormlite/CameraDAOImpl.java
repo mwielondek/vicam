ca11b58386dd056f1267a7562604a18aa1e19ad8
Fredrik Sommar <fred.sommar+github@gmail.com>
2014-05-04 19:02:47 +0200

    Deleting cameras now also delete their corresponding presets from the database.

--- camera/src/main/java/com/dreamteam/vicam/model/database/ormlite/CameraDAOImpl.java
+++ camera/src/main/java/com/dreamteam/vicam/model/database/ormlite/CameraDAOImpl.java
@@ -2,11 +2,16 @@ package com.dreamteam.vicam.model.database.ormlite;
 
 import com.dreamteam.vicam.model.database.CameraDAO;
 import com.dreamteam.vicam.model.pojo.Camera;
+import com.dreamteam.vicam.model.pojo.Preset;
+import com.dreamteam.vicam.presenter.utility.Utils;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.misc.TransactionManager;
+import com.j256.ormlite.stmt.DeleteBuilder;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import static com.dreamteam.vicam.presenter.utility.Utils.ORMLite;
 
@@ -15,10 +20,14 @@ import static com.dreamteam.vicam.presenter.utility.Utils.ORMLite;
  */
 public class CameraDAOImpl implements CameraDAO {
 
+  private final OrmLiteSqliteOpenHelper ormLiteHelper;
   private Dao<Camera, Integer> cameraDaoOrmLite;
+  private Dao<Preset, Integer> presetDaoOrmLite;
 
   public CameraDAOImpl(OrmLiteSqliteOpenHelper ormLiteHelper) throws SQLException {
+    this.ormLiteHelper = ormLiteHelper;
     this.cameraDaoOrmLite = ormLiteHelper.getDao(Camera.class);
+    this.presetDaoOrmLite = ormLiteHelper.getDao(Preset.class);
   }
 
   @Override
@@ -37,8 +46,27 @@ public class CameraDAOImpl implements CameraDAO {
   }
 
   @Override
-  public boolean deleteCamera(int id) {
-    return ORMLite.delete(cameraDaoOrmLite, id);
+  public boolean deleteCamera(final int id) {
+    try {
+      TransactionManager.callInTransaction(
+          ormLiteHelper.getConnectionSource(),
+          new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+              DeleteBuilder deleteBuilder = presetDaoOrmLite.deleteBuilder();
+              deleteBuilder.where().eq("camera_id", id);
+              deleteBuilder.delete();
+              cameraDaoOrmLite.deleteById(id);
+              return null;
+            }
+          }
+      );
+      return true;
+    } catch (SQLException e) {
+      Utils.databaseLog(String.format("Error while deleting camera with id %d", id), e);
+      return false;
+    }
+    //return ORMLite.delete(cameraDaoOrmLite, id);
   }
 
   @Override
